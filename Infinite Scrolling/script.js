d907f47465d24b5705def6618c827d69986ef5ce
Ugam Raj <ugamrajseervi1994@gmail.com>
2023-11-20 09:26:09 +0530

    updated API

--- Infinite Scrolling/script.js
+++ Infinite Scrolling/script.js
@@ -1,57 +1,70 @@
-const searchBtn = document.getElementById("searchBtn");
-const searchField = document.getElementById("searchField");
-const cards = document.getElementById("cards");
-const showMoreBtn = document.getElementById("showMoreBtn");
+const imageBox = document.getElementById("imageBox");
+const imagesContainer = document.getElementById("imagesContainer");
+const loader = document.getElementById("loader");
 
-let currPage = 1;
+// console.log(loader);
 
-showMoreBtn.style.display = "none";
+let ready = false;
+const count = 10;
+let imagesLoaded = 0;
+// const apikey = "_DDIVJSgdK-GI1wA3aHOtxC9YTt8tCY6-4jMk7guznY";
+const API_KEY = "Dm0sSUv1MLMaNgwwALWtO_CA5RpiXtka6JmcPJEB5Io";
+const URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${count}&page=1`;
 
-const displayImages = (images) => {
-  //   cards.innerHTML = "";
-  images.forEach((image) => {
-    cards.innerHTML += ` <div class="card">
-<div class="image">
-  <img
-    src="${image.urls.regular}"
-    alt="image"
-  />
-</div>
-<div class="text">
-<a href="${image.urls.full}">${image.alt_description}</a>
-</div>
-</div>`;
-  });
-
-  currPage++;
-  showMoreBtn.style.display = "none";
-
-  if (images.length >= 10) {
-    showMoreBtn.style.display = "block";
-  }
-};
-
-const getDataFromApi = async (searchKey) => {
-  const API_KEY = "Dm0sSUv1MLMaNgwwALWtO_CA5RpiXtka6JmcPJEB5Io";
-
-  const URL = `https://api.unsplash.com/search/photos?query=${searchKey}&client_id=${API_KEY}&page=${currPage}`;
-
+// const URL = `https://api.unsplash.com/search/photos?query=${searchKey}&client_id=${API_KEY}&page=${currPage}`;
+const getImages = async () => {
   try {
+    loader.style.display = "block";
     const response = await fetch(URL);
-    const data = await response.json();
-    // console.log(data.results);
-    displayImages(data.results);
-  } catch (error) {
-    console.log(error);
+    const imagesArray = await response.json();
+    displayImages(imagesArray);
+  } catch (err) {
+    console.log(err);
   }
 };
 
-const searchHandler = () => {
-  //   console.log(searchField.value);
-  const searchKey = searchField.value;
-  const result = getDataFromApi(searchKey);
+const onImageLoaded = () => {
+  imagesLoaded++;
+  if (count === imagesLoaded) {
+    // All the images have loaded
+    ready = true; // Ready to load more images
+    loader.style.display = "none";
+    // console.log("READY IS BEING SET TO TRUE");
+  }
+  // console.log("Img finished loading");
 };
 
-searchBtn.addEventListener("click", searchHandler);
+const displayImages = (imagesArray) => {
+  imagesArray.forEach((imageObject) => {
+    const imageBox = document.createElement("div");
+    imageBox.classList.add("imageBox");
+    const img = document.createElement("img");
+    img.src = imageObject.urls.regular;
+    img.alt = "Random Image";
+    // onload
+    img.addEventListener("load", onImageLoaded);
+    imageBox.appendChild(img);
+    imagesContainer.appendChild(imageBox);
+  });
+};
 
-showMoreBtn.addEventListener("click", searchHandler);
+getImages();
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY + window.innerHeight >= document.body.offsetHeight &&
+    ready === true
+  ) {
+    ready = false;
+    imagesLoaded = 0;
+    getImages();
+    // console.log("Reached bottom");
+  }
+});
+
+//! light dark mode
+
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
